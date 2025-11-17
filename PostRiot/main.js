9f751d2f4ff85c3e9310f3e5953fd16196184b48
DedZet <51733299+DedZet@users.noreply.github.com>
2025-11-17 20:04:17 +1000

    Add files via upload

---
+++ PostRiot/main.js
@@ -0,0 +1,36 @@
+let num = 2
+let text = "lol"
+
+//console.log("True")
+
+if (num == "2") {
+    console.log("0 - true\n")
+}
+
+if (num === "2") {
+    console.log("1 - true\n")
+}
+
+const a = 3;
+b = 4;
+
+// console.info("info")
+// console.log("log")
+// console.warn("warn")
+// console.debug("debug")
+// console.error("error")
+// console.clear()
+// console.time("begin")
+// console.timeEnd("begin")
+
+console.table({
+    a: 1,
+    b: 2,
+    c: 3
+})
+
+
+
+
+
+
